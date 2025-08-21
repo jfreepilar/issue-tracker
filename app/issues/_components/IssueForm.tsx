36536f259bb77454db66fba95ecfa20ff7e66d61
Joeffrey <tipsjsp@gmail.com>
2025-08-21 13:39:49 +0800

    Refactor: implement useQuery for creating and editting issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,93 +1,85 @@
-'use client';
+"use client";
 
-import { ErrorMessage, Spinner } from '@/app/components';
-import { issueSchema } from '@/app/validationIssueSchema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Issue } from '@prisma/client';
-import { Button, Callout, TextField } from '@radix-ui/themes';
-import axios from 'axios';
+import { ErrorMessage, Spinner } from "@/app/components";
+import { issueSchema } from "@/app/validationIssueSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 import { MdErrorOutline } from "react-icons/md";
-import SimpleMDE from 'react-simplemde-editor';
-import { z } from 'zod';
+import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
 
-type IssueFormData = z.infer<typeof issueSchema>
+type IssueFormData = z.infer<typeof issueSchema>;
 
-const IssueForm = ( {issue} : {issue? : Issue}) => {
-  const { 
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const {
     register,
     control,
     reset,
     handleSubmit,
-    formState: {errors, isSubmitting}
+    formState: { errors, isSubmitting },
   } = useForm({
-      resolver: zodResolver(issueSchema)
-      });
+    resolver: zodResolver(issueSchema),
+  });
 
   const router = useRouter();
-  const [error, setError] = useState('');
 
-  const onSubmit = async (data: IssueFormData) => {
-    try {
-      if (issue)
-        await axios.patch('/api/issues/' + issue.id, data)
-      else 
-        await axios.post('/api/issues', data);
-      router.push('/issues/list')
-      router.refresh()
-    } catch (error) {
-      setError('An unexpected error occured.')
-    }
-    reset();
-  }    
+  const { mutate, isError } = useMutation({
+    mutationFn: async (data: IssueFormData) => {
+      issue
+        ? await axios.patch("/api/issues/" + issue.id, data)
+        : await axios.post("/api/issues", data);
+    },
+    onSuccess: () => {
+      router.push("/issues/list");
+      router.refresh();
+      reset();
+    },
+  });
+
   return (
-    <div className='max-w-xl'>
-      {error &&
-        <Callout.Root color='red' 
-                      className='mb-5'
-        >
+    <div className="max-w-xl">
+      {isError && (
+        <Callout.Root color="red" className="mb-5">
           <Callout.Icon>
             <MdErrorOutline />
           </Callout.Icon>
-          <Callout.Text>
-            {error}
-          </Callout.Text>
+          <Callout.Text>An unexpected error occurred.</Callout.Text>
         </Callout.Root>
-      }
+      )}
 
-      <form className='space-y-3'
-        onSubmit={handleSubmit(onSubmit)}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit((data) => mutate(data))}
       >
-        <TextField.Root placeholder='Title'
-                        defaultValue={issue?.title}
-                        {...register('title')}
-        >
-        </TextField.Root>
-        <ErrorMessage>
-          {errors.title?.message}
-        </ErrorMessage>
-        <Controller name='description'
+        <TextField.Root
+          placeholder="Title"
+          defaultValue={issue?.title}
+          {...register("title")}
+        ></TextField.Root>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
           control={control}
-          defaultValue={issue?.description  }
-          render={({ field }) =>
-            <SimpleMDE placeholder='Description'
-              {...field}
-            />
-          }
+          defaultValue={issue?.description}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
         />
-        <ErrorMessage>
-          {errors.description?.message}
-        </ErrorMessage>
-        <Button disabled={isSubmitting}>
-        { issue ? 'Update Issue' : 'Submit New Issue'}{''}
-        {isSubmitting && <Spinner/> }
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting} className="!cursor-pointer">
+          {issue ? "Update Issue" : "Submit New Issue"}
+          {""}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default IssueForm
+export default IssueForm;
