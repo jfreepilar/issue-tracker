82e6730025f79d2a400400318a552236d7510478
Joeffrey <tipsjsp@gmail.com>
2025-08-21 21:49:29 +0800

    Assign Issue to user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,45 +1,52 @@
-'use client';
+"use client";
 
-import { Skeleton } from '@/app/components';
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { Skeleton } from "@/app/components";
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-
-const AssigneeSelect = () => {
-  const {data: users, error, isLoading} = useQuery({
-    queryKey: ['users'] ,
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["users"],
     queryFn: async () => {
-      const res = await axios.get<User[]>('/api/users')
-      return res.data
+      const res = await axios.get<User[]>("/api/users");
+      return res.data;
     },
     staleTime: 1000 * 60,
-    retry: 2
-  })
+    retry: 2,
+  });
 
   if (error) return null;
-  if (isLoading) return <Skeleton height='2rem'/>
-
+  if (isLoading) return <Skeleton height="2rem" />;
 
   return (
-    <Select.Root>
-      <Select.Trigger placeholder='Assign...'>
-      </Select.Trigger>
-      <Select.Content position='popper'>
+    <Select.Root
+      defaultValue={issue.assignedToUserId || "null"}
+      onValueChange={(userId) => {
+        axios.patch("/api/issues/" + issue.id, {
+          assignedToUserId: userId === "null" ? null : userId,
+        });
+      }}
+    >
+      <Select.Trigger placeholder="Assign..."></Select.Trigger>
+      <Select.Content position="popper">
         <Select.Group>
           <Select.Label>Suggestion...</Select.Label>
-            {users?.map( (user) => 
-              (<Select.Item key={user.id} 
-                            value={user.id}
-               >
-                {user.name}
-              </Select.Item>)
-            )}
-        </Select.Group> 
+          <Select.Item value="null">Unassigned</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
       </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,40 +1,45 @@
-import authOptions from '@/app/auth/authOptions';
-import { prisma } from '@/prisma/client';
-import { Box, Flex, Grid } from '@radix-ui/themes';
-import { getServerSession } from 'next-auth';
-import { notFound } from 'next/navigation';
-import DeleteIssueButton from './DeleteIssueButton';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
-import AssigneeSelect from './AssigneeSelect';
+import authOptions from "@/app/auth/authOptions";
+import { prisma } from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import DeleteIssueButton from "./DeleteIssueButton";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import AssigneeSelect from "./AssigneeSelect";
 
-
-const IssueDetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const {id} = await params
+const IssueDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
   const issue = await prisma.issue.findUnique({
-      where: { id }
-  })
+    where: { id },
+  });
 
-  if (!issue)  notFound();
+  if (!issue) notFound();
 
   const session = await getServerSession(authOptions);
 
   return (
-    <Grid columns={{ initial: "1", sm: "5",  }} gap="5" >
-      <Box className='md:col-span-4'>
-        <IssueDetails issue={issue}/>
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
       </Box>
 
-      {session && <Box>
-        <Flex direction='column' gap='4' >
-          <AssigneeSelect/>
-          <EditIssueButton issueId={issue.id}/>
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
-  )
-}
+  );
+};
 
-export const dynamic = 'force-dynamic';
-export default IssueDetailPage
+export const dynamic = "force-dynamic";
+export default IssueDetailPage;
