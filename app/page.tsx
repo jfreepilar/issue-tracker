474e49f2b73e1c824d3bb04471a04bcc7c700ea6
Joeffrey <tipsjsp@gmail.com>
2025-09-06 10:21:37 +0800

    Build the issue summary page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { prisma } from "@/prisma/client";
 import LatestIssueComponent from "./LatestIssueComponent";
+import IssueSummaryPage from "./IssueSummaryPage";
 
-const Home = () => {
+const Home = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
   return (
     <div>
-      <LatestIssueComponent />
+      {/* <LatestIssueComponent /> */}
+      <IssueSummaryPage open={open} inProgress={inProgress} closed={closed} />
     </div>
   );
 };
