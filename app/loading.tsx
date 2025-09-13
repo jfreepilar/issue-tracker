7f791657f5bf81043002f1d01171db039ed959b5
Joeffrey <tipsjsp@gmail.com>
2025-09-13 21:07:42 +0800

    Build global loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,12 @@
-import { Spinner } from "@radix-ui/themes";
-import React from "react";
+import { Flex } from "@radix-ui/themes";
+import { Roller } from "react-css-spinners";
 
 const loading = () => {
   return (
-    <div>
-      <Spinner size="3" />
-    </div>
+    <Flex justify="center" pt="184px" position="relative">
+      <Roller color="var(--violet-11)" size={100} />
+      <p className="absolute top-55 font-bold text-[14px]">Loading...</p>
+    </Flex>
   );
 };
 
