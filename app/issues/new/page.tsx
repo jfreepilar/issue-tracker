4a084b10a29021e2990ba28a83b61650284f03a4
Joeffrey <tipsjsp@gmail.com>
2025-09-18 09:26:12 +0800

    Fix: prevent "document is not defined" by disabling ssr for IssueForm

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,11 @@
-import dynamic from "next/dynamic"
-import IssueFormSkeleton from "../_components/IssueFormSkeleton"
+import dynamic from "next/dynamic";
 
-const IssueForm = dynamic(
-  () => import('@/app/issues/_components/IssueForm'),
-)
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+});
 
 const NewIssuePage = () => {
+  return <IssueForm />;
+};
 
-  return (
-    <IssueForm />
-  )
-}
-
-export default NewIssuePage
+export default NewIssuePage;
